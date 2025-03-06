2711b43f35396081bc8f0647f4a15094ca934248
Pelinsarix <gabriel.pelinsari.projetos@gmail.com>
2025-03-06 16:32:31 -0300

    Update gameConfig.js

--- frontend/assets/js/gameConfig.js
+++ frontend/assets/js/gameConfig.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'http://localhost:8000'; // URL do backend
+export const BACKEND_URL = 'https://themind-uji9.onrender.com'; // URL do backend
 export const GAME_REFRESH_INTERVAL = 300; // ms
